85fa54bd194abc279504bfcdd6fe9bb343770874
viniciusfonn <38365305+viniciusfonn@users.noreply.github.com>
2019-09-14 22:10:31 -0300

    Updated app.js, cors removed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import cors from 'cors';
 import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
-    this.server.use(cors);
     this.middlewares();
     this.routes();
   }
